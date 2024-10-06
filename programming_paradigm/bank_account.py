7e21707d10c950b52937ca8cc81e21c2072952b7
yazzy01 <rziguiyassir@gmail.com>
2024-10-06 11:05:30 -0700

    Add BankAccount class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_number, balance=0):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("Insufficient funds")
+        self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
